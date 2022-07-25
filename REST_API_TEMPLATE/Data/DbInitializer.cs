f304cb08f7b85f187feb8a57896dcabbcd268e7b
Christian Schou <christian.schou@hotmail.com>
2022-07-25 13:44:20 +0200

    Fixed DbSeeder and Added migrations for Web API

--- REST_API_TEMPLATE/Data/DbInitializer.cs
+++ REST_API_TEMPLATE/Data/DbInitializer.cs
@@ -14,55 +14,58 @@ namespace REST_API_TEMPLATE.Data
 
         public void Seed()
         {
-            _builder.Entity<Author>().HasData(
-                new Author()
+            _builder.Entity<Author>(a =>
+            {
+                a.HasData(new Author
                 {
-                    Id = new Guid(),
+                    Id = new Guid("90d10994-3bdd-4ca2-a178-6a35fd653c59"),
                     Name = "J.K. Rowling",
                     DateOfBirth = new DateTime(1965, 07, 31),
-                    Books = new List<Book>()
-                    {
-                        new Book()
-                        {
-                             Id = new Guid(),
-                             Title = "Harry Potter and the Sorcerer's Stone",
-                             Description = "Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs.",
-                             Genre = Genre.Fantasy,
-                             Publisher = "Scholastic; 1st Scholastic Td Ppbk Print., Sept.1999 edition (September 1, 1998)",
-                             ISBN = "978-0439708180",
-                             Rating = 5
-                        },
-                        new Book()
-                        {
-                            Id = new Guid(),
-                            Title = "Harry Potter and the Chamber of Secrets",
-                            Description = "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean and hideous that all Harry wanted was to get back to the Hogwarts School for Witchcraft and Wizardry. ",
-                            Genre = Genre.Fantasy,
-                            Publisher = "Scholastic Paperbacks; Reprint edition (September 1, 2000)",
-                            ISBN = "978-0439064873",
-                            Rating = 5
-                        }
-                    }
-                },
-                new Author()
+                });
+                a.HasData(new Author
                 {
-                    Id = new Guid(),
+                    Id = new Guid("6ebc3dbe-2e7b-4132-8c33-e089d47694cd"),
                     Name = "Walter Isaacson",
                     DateOfBirth = new DateTime(1952, 05, 20),
-                    Books = new List<Book>()
-                    {
-                        new Book()
-                        {
-                            Id = new Guid(),
-                            Title = "Steve Jobs",
-                            Description = "Walter Isaacson’s “enthralling” (The New Yorker) worldwide bestselling biography of Apple cofounder Steve Jobs.",
-                            Genre = Genre.Biography,
-                            Publisher = "Simon & Schuster; 1st edition (October 24, 2011)",
-                            ISBN = "978-1451648539",
-                            Rating = 4.5
-                        }
-                    }
                 });
+            });
+
+            _builder.Entity<Book>(b =>
+            {
+                b.HasData(new Book
+                {
+                    Id = new Guid("98474b8e-d713-401e-8aee-acb7353f97bb"),
+                    Title = "Harry Potter and the Sorcerer's Stone",
+                    Description = "Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs.",
+                    Genre = Genre.Fantasy,
+                    Publisher = "Scholastic; 1st Scholastic Td Ppbk Print., Sept.1999 edition (September 1, 1998)",
+                    ISBN = "978-0439708180",
+                    Rating = 5,
+                    AuthorId = new Guid("90d10994-3bdd-4ca2-a178-6a35fd653c59")
+                });
+                b.HasData(new Book
+                {
+                    Id = new Guid("bfe902af-3cf0-4a1c-8a83-66be60b028ba"),
+                    Title = "Harry Potter and the Chamber of Secrets",
+                    Description = "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean and hideous that all Harry wanted was to get back to the Hogwarts School for Witchcraft and Wizardry. ",
+                    Genre = Genre.Fantasy,
+                    Publisher = "Scholastic Paperbacks; Reprint edition (September 1, 2000)",
+                    ISBN = "978-0439064873",
+                    Rating = 5,
+                    AuthorId = new Guid("90d10994-3bdd-4ca2-a178-6a35fd653c59")
+                });
+                b.HasData(new Book
+                {
+                    Id = new Guid("150c81c6-2458-466e-907a-2df11325e2b8"),
+                    Title = "Steve Jobs",
+                    Description = "Walter Isaacson’s “enthralling” (The New Yorker) worldwide bestselling biography of Apple cofounder Steve Jobs.",
+                    Genre = Genre.Biography,
+                    Publisher = "Simon & Schuster; 1st edition (October 24, 2011)",
+                    ISBN = "978-1451648539",
+                    Rating = 4.5,
+                    AuthorId = new Guid("6ebc3dbe-2e7b-4132-8c33-e089d47694cd")
+                });
+            });
         }
     }
 }
---
+++ REST_API_TEMPLATE/Migrations/20220725111354_initial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace REST_API_TEMPLATE.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Subtitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<int>(type: "int", nullable: true),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { new Guid("6ebc3dbe-2e7b-4132-8c33-e089d47694cd"), new DateTime(1952, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Walter Isaacson" });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { new Guid("90d10994-3bdd-4ca2-a178-6a35fd653c59"), new DateTime(1965, 7, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "J.K. Rowling" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Description", "Genre", "ISBN", "Publisher", "Rating", "ReleaseDate", "Subtitle", "Title" },
+                values: new object[] { new Guid("150c81c6-2458-466e-907a-2df11325e2b8"), new Guid("6ebc3dbe-2e7b-4132-8c33-e089d47694cd"), "Walter Isaacson’s “enthralling” (The New Yorker) worldwide bestselling biography of Apple cofounder Steve Jobs.", 25, "978-1451648539", "Simon & Schuster; 1st edition (October 24, 2011)", 4.5, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Steve Jobs" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Description", "Genre", "ISBN", "Publisher", "Rating", "ReleaseDate", "Subtitle", "Title" },
+                values: new object[] { new Guid("98474b8e-d713-401e-8aee-acb7353f97bb"), new Guid("90d10994-3bdd-4ca2-a178-6a35fd653c59"), "Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs.", 0, "978-0439708180", "Scholastic; 1st Scholastic Td Ppbk Print., Sept.1999 edition (September 1, 1998)", 5.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Harry Potter and the Sorcerer's Stone" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Description", "Genre", "ISBN", "Publisher", "Rating", "ReleaseDate", "Subtitle", "Title" },
+                values: new object[] { new Guid("bfe902af-3cf0-4a1c-8a83-66be60b028ba"), new Guid("90d10994-3bdd-4ca2-a178-6a35fd653c59"), "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean and hideous that all Harry wanted was to get back to the Hogwarts School for Witchcraft and Wizardry. ", 0, "978-0439064873", "Scholastic Paperbacks; Reprint edition (September 1, 2000)", 5.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Harry Potter and the Chamber of Secrets" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
